d2cf33f42b116ae17aaddbc8453a9ccf5140d5c3
pkseth <princeseth708@gmail.com>
2020-03-20 17:40:14 +0530

    TestNG Jar

---
+++ src/test/java/mavenPkg/NewTest.java
@@ -0,0 +1,28 @@
+package mavenPkg;
+
+
+import org.testng.annotations.Test;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewTest {
+
+	@Test
+	public void EnvironmentSetup() throws IOException
+	{ 	
+	  	
+	  	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"./WebDriver/"+"chromedriver.exe");		
+	  	WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		String Title=driver.getTitle();
+		if (Title.contains("Google")){
+			System.out.println("Verified Title Successfully");
+			driver.close();
+			driver.quit();
+		}
+		
+		
+	}
+    
+}
