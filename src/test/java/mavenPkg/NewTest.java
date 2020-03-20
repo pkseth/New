45052642e05f8bb03e8f92d1daa836fbbb98ae52
pkseth <princeseth708@gmail.com>
2020-03-20 18:31:27 +0530

    TestNG Jar2

--- src/test/java/mavenPkg/NewTest.java
+++ src/test/java/mavenPkg/NewTest.java
@@ -3,19 +3,29 @@ package mavenPkg;
 
 import org.testng.annotations.Test;
 
+import bsh.Capabilities;
+
 import java.io.File;
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class NewTest {
 
 	@Test
 	public void EnvironmentSetup() throws IOException
 	{ 	
-		File file = new File("C:/Users/Prince/workspace/MavenTest/WebDriver/chromedriver.exe"); 
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Users\\Prince\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		//ChromeDriver driver = new ChromeDriver(capabilities);
+		
+		File file = new File("C:\\Users\\Prince\\workspace\\MavenTest\\WebDriver\\chromedriver.exe"); 
 	  	System.setProperty("webdriver.chrome.driver",file.getAbsolutePath());		
-	  	WebDriver driver=new ChromeDriver();
+	  	ChromeDriver driver=new ChromeDriver(capabilities);
 		driver.get("https://www.google.com");
 		String Title=driver.getTitle();
 		if (Title.contains("Google")){
