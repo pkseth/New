fbd42989af904c3aa614b374548a8c1990037951
pkseth <princeseth708@gmail.com>
2020-03-23 16:47:00 +0530

    Latested One

--- src/test/java/mavenPkg/NewTest.java
+++ src/test/java/mavenPkg/NewTest.java
@@ -17,15 +17,15 @@ public class NewTest {
 	@Test
 	public void EnvironmentSetup() throws IOException
 	{ 	
-		ChromeOptions options = new ChromeOptions();
+		/*ChromeOptions options = new ChromeOptions();
 		options.setBinary("C:/Users/Prince/AppData/Local/Google/Chrome/Application/chrome.exe");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options); */
 		//ChromeDriver driver = new ChromeDriver(capabilities);
 		
 		File file = new File("C:\\Users\\Prince\\workspace\\MavenTest\\WebDriver\\chromedriver.exe"); 
 	  	System.setProperty("webdriver.chrome.driver",file.getAbsolutePath());		
-	  	ChromeDriver driver=new ChromeDriver(capabilities);
+	  	ChromeDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com");
 		String Title=driver.getTitle();
 		if (Title.contains("Google")){
